10d3da6226b9684940d1762ccdc3e392625beeac
Lucian Necula <lknecula@gmail.com>
2022-12-21 11:23:18 +0200

    a

--- traprainwater.py
+++ traprainwater.py
@@ -1,7 +1,2 @@
-# cho "# prep-tech" >> README.md
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git branch -M main
-# git remote add origin https://github.com/lknecula/prep-tech.git
-# git push -u origin main
+def naive_solution(heights):
+    pass
